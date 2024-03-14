b754c9f8aea6d9b193f7db5bade8778c98dbfece
zp06 <108944921+zp06@users.noreply.github.com>
2024-03-14 08:19:35 -0300

    k

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
-function aperte(){
-    while(true){
-        alert("Virús detectado!");
-    }
-   
+function abrir_aba(num){
+if(num == "1"){
+    document.getElementById("btn1").disabled = true;
+}
+if(num == "2"){
+    document.getElementById("btn2").disabled = true;
+}
+if(num == "3"){
+    document.getElementById("btn3").disabled = true;
+}
+if(num == "4"){
+    document.getElementById("btn4").disabled = true;
+}
+if(num == "5"){
+    document.getElementById("btn5").disabled = true;
+}
 }
